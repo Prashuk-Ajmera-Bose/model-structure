452df6a6d575872905441a5b23b58e38340a746c
Prashuk-Ajmera-Bose <Prashuk_Ajmera@bose.com>
2019-09-03 16:41:12 -0400

    Removed file insertion code

--- Model WatchKit Extension/InterfaceController.swift
+++ Model WatchKit Extension/InterfaceController.swift
@@ -13,20 +13,21 @@ import CoreMotion
 class InterfaceController: WKInterfaceController {
     
     let cmMotion = CMMotionManager()
+    let fileName = "data"
+    let dir = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    
+    var dataList = [String]()
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
     }
     
     override func willActivate() {
         super.willActivate()
-        
     }
     
     override func didDeactivate() {
         super.didDeactivate()
-        
     }
     
     // MARK: User Defined Functions
@@ -37,8 +38,19 @@ class InterfaceController: WKInterfaceController {
            self.cmMotion.showsDeviceMovementDisplay = true
            self.cmMotion.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error) in
               if let newData = data {
-//                print(newData)
-                print(newData.userAcceleration.x)
+                print(newData.userAcceleration, newData.rotationRate)
+                let dataStr = String(describing: newData)
+//                self.dataList.append(contentsOf: dataStr.map(String.init))
+//                print(self.dir)
+//                if let fileURL = self.dir?.appendingPathComponent(self.fileName).appendingPathExtension("txt") {
+//                    let outString = dataStr
+//                    do {
+//                        try outString.write(to: fileURL, atomically: true, encoding: .utf8)
+//                    } catch {
+//                        print("Failed writing to URL: \(fileURL), Error: " + error.localizedDescription)
+//                    }
+//                }
+                
               }
            }
         }
